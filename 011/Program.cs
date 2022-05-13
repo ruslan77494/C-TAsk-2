2b71db3bbc5a50c4e7799f6e52cded31794dad48
ruslan <rus.pro94@bk.ru>
2022-05-13 14:44:15 +0500

    011

---
+++ 011/Program.cs
@@ -0,0 +1,9 @@
+﻿// : Напишите программу, которая выводит случайное трёхзначное число и удаляет вторую цифру этого числа. 456 -> 46 782 -> 72
+
+int number = new Random().Next(10,1000);
+
+string strr = number.ToString();
+
+Console.WriteLine($"{number} -> {strr[0]}{strr[2]}");
+
+
