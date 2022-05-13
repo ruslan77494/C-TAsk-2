cd901ea2bef7c1265606fa63a08c17b17a0607af
ruslan <rus.pro94@bk.ru>
2022-05-13 15:18:56 +0500

    022

---
+++ 022/Program.cs
@@ -0,0 +1,21 @@
+﻿/* Напишите программу, которая будет принимать на вход два числа и выводить, является ли второе число кратным первому. 
+Если число 2 не кратно числу 1, то 
+программа выводит остаток от деление.
+34, 5 -> не кратно, остаток 4
+16, 4 -> кратно
+1*/
+
+Console.WriteLine(" Пожалуйста Введите Первое Число");
+string fistnNumberStr = Console.ReadLine();
+Console.WriteLine("Пожалуйста Введите Второе  Число");
+string secondNumberStr = Console.ReadLine();
+
+int first = int.Parse(fistnNumberStr);
+int second = int.Parse(secondNumberStr);
+int result = first % second;
+
+if(result == 0)
+    Console.WriteLine("кратно");
+else
+    Console.WriteLine($"Не кратно, остаток {result}");
+
